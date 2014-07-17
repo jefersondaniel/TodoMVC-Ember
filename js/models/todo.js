6d3aaa571a3d74304d4a758e70b742cad9c0c3ff
Jeferson Daniel <jeferson.daniel412@gmail.com>
2014-07-17 01:01:48 -0300

    Initial commit

---
+++ js/models/todo.js
@@ -0,0 +1,22 @@
+Todos.Todo = DS.Model.extend({
+    title: DS.attr('string'),
+    isCompleted: DS.attr('boolean'),
+});
+
+Todos.Todo.FIXTURES = [
+    {
+        id: 1,
+        title: 'Aprender Ember.js',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        title: '...',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        title: 'Aproveitar',
+        isCompleted: false
+    }
+];
